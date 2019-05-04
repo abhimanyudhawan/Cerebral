890ce711de5e44632b75eec2212096613cf0a54c
Abhimanyu Dhawan <abhimanyudhawan@Abhimanyus-Air.lan1>
2019-05-04 18:47:02 -0400

    corrected launch in main

--- server_code/main.py
+++ server_code/main.py
@@ -74,5 +74,5 @@ def scan_process():
         return jsonify(**res)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
-#app.run()
+    # app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run()
